bd4cf62bab695c9ef7697ca6dd0c1721413d4f48
muit <miguel_3c@hotmail.com>
2015-07-07 16:53:32 +0200

    Bot created

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'zDixHmai0n80oa6g4lXTGLcav',
+    consumer_secret: 'QDkSIu3Z9tu3Syu4Sdq7aLpXVilbC6U1YY1jDHauBmWNHCs54x',
+    access_token: '3362263109-yXRkt5zRhdUqC37zdSnPivPvbcwwoaqlVjF7qr6',
+    access_token_secret: '2iddxACNgdHVB913KupAWiq2fxgr2bzM7OezPmOboTlCz'
+  },
+
+  autoFollow: true,
+  autoRetweet: true,
+
+  markov: {
+    separator: "",
+    order: 2
+  }
+}
