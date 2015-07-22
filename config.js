cae1c2091353d3a13e97e13510d9013f7e27467a
muit <miguel_3c@hotmail.com>
2015-07-22 15:55:51 +0200

    Prod_AddConfig

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'zDixHmai0n80oa6g4lXTGLcav',
+    consumer_secret: 'QDkSIu3Z9tu3Syu4Sdq7aLpXVilbC6U1YY1jDHauBmWNHCs54x',
+    access_token: '3362263109-yXRkt5zRhdUqC37zdSnPivPvbcwwoaqlVjF7qr6',
+    access_token_secret: '2iddxACNgdHVB913KupAWiq2fxgr2bzM7OezPmOboTlCz'
+  },
+
+  autoFollow: true,
+  autoRetweet: true,
+
+  markov: {
+    enabled: true,
+    separator: " ",
+    order: 1
+  },
+
+  lang: 'es, en'
+}
